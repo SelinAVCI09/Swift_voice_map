dd2aa7de32fddc421754775a8793bdf39a5d442f
SelinAVCI09 <121114182+SelinAVCI09@users.noreply.github.com>
2024-05-24 21:50:13 +0300

    değişiklik

--- Swift_voice_map/MyTrips/Destinations Tab/DestinationLocationsMapView.swift
+++ Swift_voice_map/MyTrips/Destinations Tab/DestinationLocationsMapView.swift
@@ -1,13 +1,3 @@
-//
-// Created for MyTrips
-// by  Stewart Lynch on 2023-12-31
-//
-// Follow me on Mastodon: @StewartLynch@iosdev.space
-// Follow me on Threads: @StewartLynch (https://www.threads.net)
-// Follow me on X: https://x.com/StewartLynch
-// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
-// Subscribe on YouTube: https://youTube.com/@StewartLynch
-// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
 
 
 import SwiftUI
